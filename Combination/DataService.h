366af99ccb4848f648b6b270a5478e2552e40f4a
alecchyi <alecchyi@gmail.com>
2014-07-15 17:22:32 +0800

    add data service

---
+++ Combination/DataService.h
@@ -0,0 +1,13 @@
+//
+//  DataService.h
+//  Combination
+//
+//  Created by Ruby on 14-7-15.
+//  Copyright (c) 2014年 ___alecchyi___. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataService : NSObject
+
+@end
---
+++ Combination/DataService.m
@@ -0,0 +1,13 @@
+//
+//  DataService.m
+//  Combination
+//
+//  Created by Ruby on 14-7-15.
+//  Copyright (c) 2014年 ___alecchyi___. All rights reserved.
+//
+
+#import "DataService.h"
+
+@implementation DataService
+
+@end
